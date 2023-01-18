67c3e1c2c6dea336362ae612b745c8ebc2d97f21
Mohammad Turaab <45646831+mohammadturaab@users.noreply.github.com>
2023-01-17 22:01:53 -0600

    added rest framework

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serliaizers import TodoSerializer
+from .models import Todo
+
 
 # Create your views here.
+class TodoView(viewsets.ModelView):
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
